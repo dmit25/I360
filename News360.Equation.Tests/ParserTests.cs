8ab5df4ca54f4284abdd9c923997c23a477148fc
Dmitry Zobnin <dmit25@hotmail.com>
2016-08-12 22:47:40 +0300

    some tests added, still figuring out DC

---
+++ News360.Equation.Tests/ParserTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace News360.Equation.Tests
+{
+    public static class Ext
+    {
+        public static Variable V(this string name, double factor = 1.0, int power = 1)
+        {
+            return new Variable { Factor = factor, Name = name, Power = power };
+        }
+
+        public static Brackets B(this string name, double factor, params Member[] members)
+        {
+            return new Brackets
+            {
+                Name = name,
+                Factor = factor,
+                Content = members.ToList()
+            };
+        }
+
+        public static Equation L(params Member[] members)
+        {
+            return new Equation { LeftPart = members.ToList() };
+        }
+
+        public static Equation R(this Equation equation, params Member[] members)
+        {
+            equation.RightPart = members.ToList();
+            return equation;
+        }
+    }
+
+    [TestFixture]
+    public class ParserTests
+    {
+        public IEnumerable<KeyValuePair<string, Equation>> Cases()
+        {
+            return new Dictionary<string, Equation>
+            {
+                { "x^2 + 3.5xy + y = y^2 - xy + y",
+                    Ext.L("x".V(1,2),"xy".V(3.5),"y".V())
+                    .R("y".V(1,2),"xy".V(-1),"y".V())
+                },
+
+                { "x^2 + 3.5x(y + y) = y^2( - xy + y)",
+                    Ext.L("x".V(1,2),"x".B(3.5,"y".V(),"y".V()))
+                    .R("y".V(1,2),"xy".V(-1),"y".V())
+                },
+            };
+        }
+
+        [Test, TestCaseSource(nameof(Cases))]
+        public void NAME()
+        {
+
+        }
+    }
+}
---
+++ News360.Equation/Equation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace News360.Equation
+{
+    public class Equation
+    {
+        public List<Member> LeftPart { get; set; }
+        public List<Member> RightPart { get; set; }
+    }
+
+    public abstract class Member
+    {
+        public double Factor { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Brackets:Member
+    {
+        public List<Member> Content { get; set; }
+    }
+
+    public class Variable : Member
+    {
+        public int Power { get; set; }
+    }
+
+    public class Parser
+    {
+        public Equation Parse(string input)
+        {
+            return new Equation();
+        }
+    }
+
+    public class Canonizer
+    {
+        public void Canonize(ref Equation equation)
+        {
+
+        }
+    }
+
+    public class Renderer
+    {
+        public string Render(Equation equation) { return string.Empty; }
+    }
+}
